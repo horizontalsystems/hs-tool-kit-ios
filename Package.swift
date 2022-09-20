d3a4df65643b1f9cada82b11953871c1273fb7a2
max <trickster77777@gmail.com>
2022-09-20 18:13:39 +0600

    Add SPMability

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "HsToolKit",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "HsToolKit",
+            targets: ["HsToolKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.1")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .exact("6.5.0")),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "4.1.0")),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+
+    ],
+    targets: [
+        .target(
+            name: "HsToolKit",
+            dependencies: [
+                "Alamofire",
+                "RxSwift",
+                "ObjectMapper",
+                .product(name: "NIOWebSocket", package: "swift-nio"),
+                .product(name: "NIOSSL", package: "swift-nio"),
+                .product(name: "NIOFoundationCompat", package: "swift-nio"),
+            ]),
+    ]
+)
