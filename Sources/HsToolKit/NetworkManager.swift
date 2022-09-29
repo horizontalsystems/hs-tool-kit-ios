825c12135ae0b0d5055572a5696b6f9290c316f7
max <trickster77777@gmail.com>
2022-09-29 18:14:01 +0600

    Revert "Fix imports"
    This reverts commit 07db2236bad1cf65f6b6e3851f1e998a9f0de7fa.

--- Sources/HsToolKit/NetworkManager.swift
+++ Sources/HsToolKit/NetworkManager.swift
@@ -1,4 +1,3 @@
-import Foundation
 import RxSwift
 import Alamofire
 
@@ -29,7 +28,7 @@ public class NetworkManager {
                 case .success(let result):
                     observer(.success(result))
                 case .failure(let error):
-                    observer(.failure(NetworkManager.unwrap(error: error)))
+                    observer(.error(NetworkManager.unwrap(error: error)))
                 }
 
                 if let postDelay = postDelay {
@@ -57,7 +56,7 @@ public class NetworkManager {
 
         return Single<Mapper.T>.create { [weak self] observer in
             guard let manager = self else {
-                observer(.failure(NetworkManager.RequestError.disposed))
+                observer(.error(NetworkManager.RequestError.disposed))
                 return Disposables.create()
             }
 
@@ -76,7 +75,7 @@ public class NetworkManager {
                 case .success(let result):
                     observer(.success(result))
                 case .failure(let error):
-                    observer(.failure(NetworkManager.unwrap(error: error)))
+                    observer(.error(NetworkManager.unwrap(error: error)))
                 }
             }
 
